defe409d78433b322c83842b060c15c2063e7f5a
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-01-24 11:43:54 +0100

    Added a comment about the includeStr "macro"

--- src/Compiler/TH.hs
+++ src/Compiler/TH.hs
@@ -4,7 +4,12 @@ import Control.Monad
 import Language.Haskell.TH
 import System.IO.Unsafe(unsafePerformIO)
 
+-- While this is hacky (specifically the use of unsafePerformIO)
+-- in this case I think it is fine, as if an invalid string
+-- is passed to the function it will fail to compile,
+-- which is the intended behavior. This allows us to 
+-- import strings (such as our "standard LLVM library")
+-- during compile time, removing the need to ship the source for 
+-- that with the compiler. 
 includeStr :: String -> Q Exp
-includeStr file = do
-    let res = unsafePerformIO $ readFile file
-    [|res|]
+includeStr file = [|unsafePerformIO $ readFile file|]
