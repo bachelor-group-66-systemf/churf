7d91cf871abf4ec2f6415469368b79336b9bc0ad
Victor Olin <victor.vieweg@icloud.com>
2023-02-10 13:47:09 +0100

    Goal for next week

---
+++ src/GC/allocator.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <assert.h>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+#include "include/allocator.hpp"
+
+namespace GC {
+
+
+
+
+}
--- src/GC/include/heap_obj.hpp
+++ src/GC/include/heap_obj.hpp
@@ -4,13 +4,22 @@
 
 class HeapObj {
 public:
-    HeapObj(size_t size_) {
+    HeapObj(void *start_, size_t size_) {
+        start = start_;
         size = size_;
     }
 
     ~HeapObj() { }
 
+    void *getAddr() { return start; }
+
+    size_t getSize() { return size; }
+
+    bool isMarked() { return marked; }
+    void mark() { marked = true; }
+
 private:
+    void *start = 0;
     size_t size { 0 };
     bool marked { false };
 };
