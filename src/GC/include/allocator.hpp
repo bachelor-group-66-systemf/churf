e52c38f5f7d603adaa10cabb168569d5d5be691d
Victor Olin <victor.vieweg@icloud.com>
2023-02-10 13:45:53 +0100

    Worked a bit on the heap

--- src/GC/include/allocator.hpp
+++ src/GC/include/allocator.hpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include <stdlib.h>
 
+#include "include/heap.hpp"
+
 namespace GC {
 
 class Allocator {
@@ -12,11 +14,18 @@ public:
         alloc_size = size;
     }
 
+    ~Allocator() { }
+
     size_t getSize() {
         return alloc_size;
     }
 
-    void *alloc();
+    void *alloc() {
+        auto heap = Heap::the();
+
+        assert(heap.getHeapSize() + alloc_size <= HEAP_SIZE);
+
+    }
 
 private:
     size_t alloc_size;
