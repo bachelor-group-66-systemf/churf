514c809b1ea8e29d46d05058ccef26984ba3c2ca
Rakarake <williambodin2001@gmail.com>
2023-02-14 15:47:36 +0100

    Pinned nix channel, made makefile not error

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import <nixpkgs> { }; # pin the channel to ensure reproducibility!
+  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/8c619a1f3cedd16ea172146e30645e703d21bfc1.tar.gz) { }; # pin the channel to ensure reproducibility!
 in
 pkgs.haskellPackages.developPackage {
   root = ./.;
