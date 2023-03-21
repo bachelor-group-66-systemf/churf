edeff09e866077bb260c0ca44fdc82fda978602b
valtermiari <valter@miari.se>
2023-03-21 13:11:58 +0100

    Started skeleton for accurate GC

---
+++ src/Accurate_GC/gc.cpp
@@ -0,0 +1,16 @@
+/* // TODO: include these properly
+#include "llvm/CodeGen/GCStrategy.h"
+#include "llvm/CodeGen/GCMetadata.h"
+#include "llvm/Support/Compiler.h"
+
+using namespace llvm;
+
+namespace {
+  class LLVM_LIBRARY_VISIBILITY GC : public GCStrategy {
+  public:
+    GC() {}
+  };
+
+  GCRegistry::Add<GC>
+  X("gc", "The bespoken garbage collector.");
+} */
