92a2ff323565385a06652af12e4edfa81bbdc0bf
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-03-28 17:13:38 +0200

    Fixed a broken path.

--- src/Main.hs
+++ src/Main.hs
@@ -2,43 +2,34 @@
 
 module Main where
 
-import Codegen.Codegen (generateCode)
-import Compiler (compile)
-import Control.Monad (when)
-import Data.Bool (bool)
-import Data.List.Extra (isSuffixOf)
-import Data.Maybe (fromJust, isNothing)
-import GHC.IO.Handle.Text (hPutStrLn)
-import Grammar.ErrM (Err)
-import Grammar.Par (myLexer, pProgram)
-import Grammar.Print (printTree)
-import LambdaLifter (lambdaLift)
-import Monomorphizer.Monomorphizer (monomorphize)
-import Renamer.Renamer (rename)
-import System.Console.GetOpt (
-    ArgDescr (NoArg, ReqArg),
-    ArgOrder (RequireOrder),
-    OptDescr (Option),
-    getOpt,
-    usageInfo,
- )
-import System.Directory (
-    createDirectory,
-    doesPathExist,
-    getDirectoryContents,
-    removeDirectoryRecursive,
-    setCurrentDirectory,
- )
-import System.Environment (getArgs)
-import System.Exit (
-    ExitCode (ExitFailure),
-    exitFailure,
-    exitSuccess,
-    exitWith,
- )
-import System.IO (stderr)
-import System.Process (spawnCommand, waitForProcess)
-import TypeChecker.TypeChecker (TypeChecker (Bi, Hm), typecheck)
+import           Codegen.Codegen             (generateCode)
+import           Compiler                    (compile)
+import           Control.Monad               (when)
+import           Data.Bool                   (bool)
+import           Data.List.Extra             (isSuffixOf)
+import           Data.Maybe                  (fromJust, isNothing)
+import           GHC.IO.Handle.Text          (hPutStrLn)
+import           Grammar.ErrM                (Err)
+import           Grammar.Par                 (myLexer, pProgram)
+import           Grammar.Print               (printTree)
+import           LambdaLifter                (lambdaLift)
+import           Monomorphizer.Monomorphizer (monomorphize)
+import           Renamer.Renamer             (rename)
+import           System.Console.GetOpt       (ArgDescr (NoArg, ReqArg),
+                                              ArgOrder (RequireOrder),
+                                              OptDescr (Option), getOpt,
+                                              usageInfo)
+import           System.Directory            (createDirectory, doesPathExist,
+                                              getDirectoryContents,
+                                              removeDirectoryRecursive,
+                                              setCurrentDirectory)
+import           System.Environment          (getArgs)
+import           System.Exit                 (ExitCode (ExitFailure),
+                                              exitFailure, exitSuccess,
+                                              exitWith)
+import           System.IO                   (stderr)
+import           System.Process              (spawnCommand, waitForProcess)
+import           TypeChecker.TypeChecker     (TypeChecker (Bi, Hm), typecheck)
 
 main :: IO ()
 main = getArgs >>= parseArgs >>= uncurry main'
@@ -85,11 +76,11 @@ chooseTypechecker s options = options{typechecker = tc}
     tc = case s of
         "hm" -> pure Hm
         "bi" -> pure Bi
-        _ -> Nothing
+        _    -> Nothing
 
 data Options = Options
-    { help :: Bool
-    , debug :: Bool
+    { help        :: Bool
+    , debug       :: Bool
     , typechecker :: Maybe TypeChecker
     }
 
@@ -128,7 +119,7 @@ main' opts s = do
         debugDotViz
 
     compile generatedCode
-    spawnWait "./hello_world"
+    spawnWait "./output/hello_world"
 
     exitSuccess
 
