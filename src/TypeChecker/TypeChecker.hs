698e23e8453f4b1aec310a415a24cd8610776306
sebastianselander <sebastian.selander@gmail.com>
2023-04-29 17:35:43 +0200

    commented out bidir as it is not adapted yet

--- src/TypeChecker/TypeChecker.hs
+++ src/TypeChecker/TypeChecker.hs
@@ -4,7 +4,8 @@ import Control.Monad ((<=<))
 import Grammar.Abs
 import Grammar.ErrM (Err)
 import TypeChecker.RemoveTEVar (RemoveTEVar (rmTEVar))
-import TypeChecker.TypeCheckerBidir qualified as Bi
+
+-- import TypeChecker.TypeCheckerBidir qualified as Bi
 import TypeChecker.TypeCheckerHm qualified as Hm
 import TypeChecker.TypeCheckerIr qualified as T
 
@@ -14,5 +15,5 @@ typecheck :: TypeChecker -> Program -> Err T.Program
 typecheck tc = rmTEVar <=< f
   where
     f = case tc of
-        Bi -> Bi.typecheck
+        -- Bi -> Bi.typecheck
         Hm -> Hm.typecheck
